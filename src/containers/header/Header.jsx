fdf5261757071d95dc34696783b60adae9c9b13e
Eke-sims <Eketimileyin@gmail.com>
2022-07-26 10:36:04 +0100

    made the navbar and header responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,41 +1,42 @@
 import './header.css'
 import banner from '../../assets/Group 3.png'
-import play from '../../assets/Group 1.png'
 
 const Header = () => {
   return (
-    <div id='Header' className='header-container section-container'>
+    <section>
+      <div id='Header' className='header-container section-container'>
 
-      <div className="header-content">
+        <div className="header-content">
 
-        <h4>Best Destinations around the world</h4>
-        <h1>
-          Travel, enjoy
-          and live a new
-          and full life
-        </h1>
-        <p>Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
+          <h4>Best Destinations around the world</h4>
+          <h1>
+            Travel, enjoy
+            and live a new
+            and full life
+          </h1>
+          <p>Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
 
 
-        <div className="action">
+          <div className="action">
 
-          <button>Find Out More</button>
-          <div className='demo'>
-            <div className="red-btn">
-              <div className="white"></div>
+            <button>Find Out More</button>
+            <div className='demo'>
+              <div className="red-btn">
+                <div className="white"></div>
+              </div>
+              <span>Play Demo</span>
             </div>
-            <span>Play Demo</span>
+
           </div>
 
         </div>
 
-      </div>
+        <div className="image-container">
+          <img src={banner} alt="happy traveller" className='header-image'/>
+        </div>
 
-      <div className="image-container">
-        <img src={banner} alt="happy traveller" />
       </div>
-
-    </div>
+    </section>
   )
 }
 
